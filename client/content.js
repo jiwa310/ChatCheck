5e6addcf8552ba2978968bc0b79b149d17f75110
Scott Weston <scottricardo@ucsb.edu>
2023-05-14 01:22:58 -0700

    Testing

--- client/content.js
+++ client/content.js
@@ -1,20 +1,53 @@
 let timeoutId;
 let observer;
 
+const axios = require('axios');
+
+async function generateResponse(prompt) {
+  const response = await axios.post('http://localhost:5000/api/generate', { prompt });
+  return response.data.data;
+}
+
+
+
+  //-------------WHAT JIM WROTE---------------------------
+
+// async function sendRequest(textmessage) {
+//     const response = await fetch('http://localhost:5000/api/generate', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({ textmessage })
+//     });
+//     const data = await response.json();
+//     // Handle the response data here
+//   }
+
 function startObserving() {
     let textBox = document.querySelector('div[role="textbox"].markup-eYLPri');
     if (textBox) {
         let span = textBox.querySelector('div span span span');
         if (span) {
             let parentSpan = span.parentElement;
+            let grandparentSpan = span.parentElement.parentElement;
+            let img = document.createElement('img');
             observer = new MutationObserver(mutations => {
                 clearTimeout(timeoutId);
+                img.src = chrome.runtime.getURL("images/brilliant_32x.png");
+                if (grandparentSpan.contains(img)) {
+                    grandparentSpan.removeChild(img);
+                }
                 timeoutId = setTimeout(() => {
-                    console.log('span content:', span.textContent);
                     console.log('parentSpan content:', parentSpan.textContent);
-                    let img = document.createElement('img');
-                    img.src = chrome.runtime.getURL("images/brilliant_32x.png");
-                    parentSpan.appendChild(img);
+                    //SCOTT WROTE THIS-----------------------------------------------------
+                    generateResponse(parentSpan.textContent)
+                        .then(response => console.log(response))
+                        .catch(error => console.error(error));
+                    //SCOTT WROTE THIS-----------------------------------------------------
+                    //sendRequest(parentSpan.textContent); // Send the request to the server
+
+                    grandparentSpan.appendChild(img);
                 }, 2000);
             });
             observer.observe(span, { characterData: true, childList: true, subtree: true });
@@ -29,7 +62,8 @@ function restartObserving() {
 }
 
 function handleKeydown(event) {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
         restartObserving();
     }
 }
