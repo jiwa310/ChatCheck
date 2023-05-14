207cf6a543a383819e7890c1458de850f6792f26
Jim Wang <jimwang@ucsb.edu>
2023-05-14 06:56:13 -0700

    final api call

--- Server1/index.js
+++ Server1/index.js
@@ -37,8 +37,7 @@ app.post("/api/generate", async (req, res) => {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `I am a lonely person trying to charm someone I am sexually attracted to
-I am messaging this person on a dating app. Rate how good my messages are on a scale of 1-10. If below a 5/10, give 
+  return `I am messaging this person on a dating app. Rate how good my messages are on a scale of 1-10. If below a 5/10, give 
 me an example of a good line.
 
 Me: Hey, how are you?
@@ -47,10 +46,6 @@ Me: I need a map so I don't get lost in your eyes.
 Rating: 8/10. Cheesy, but funny.
 Me: I want you to sit on my face mommy.
 Rating: 1/10. Very bold, but also very inappropriate.
-Me: Damn mami, you got that fat booty. I'm trynna hit that.
-Rating: 10/10. You will definitely get a response.
-Me: I want to put my dick in your asshole.
-Rating: 10/10. Score!
 Me: ${capitalizedAnimal}
 Rating:`;
 }
