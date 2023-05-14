b5ab5b471aea1b4a54e37138fe9eecd954abfcdc
Scott Weston <scottricardo@ucsb.edu>
2023-05-14 07:39:58 -0700

    edited filepath

--- Server1/test.js
+++ Server1/test.js
@@ -1,13 +1,26 @@
 const axios = require('axios');
 
+// async function generateResponse(prompt) {
+//   const response = await axios.post('https://rizz-eval-production.up.railway.app/api/generate', { prompt });
+//   return response.data.data;
+// }
+
 async function generateResponse(prompt) {
-  const response = await axios.post('http://localhost:4000/api/generate', { prompt });
-  return response.data.data;
-}
+    const response = await fetch('https://rizz-eval-production.up.railway.app/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt })
+    });
+    const data = await response.json();
+    return data.data;
+    
+  }
 
 
 
-generateResponse("test input")
+generateResponse("I'd love to get to know you better")
   .then(response => console.log(response))
   .catch(error => console.error(error));
 
